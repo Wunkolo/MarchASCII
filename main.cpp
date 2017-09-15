2d156122d14ed6877b322e6413fbf786646faf5f
Wunkolo <Wunkolo@gmail.com>
2017-09-15 12:07:33 -0700

    Update formatting

--- main.cpp
+++ main.cpp
@@ -41,25 +41,25 @@ glm::vec4 Scene(const glm::vec3& Point)
 
 	Query = Operations::Union(
 		glm::vec4(1, 0, 0, Shapes::Box(
-				glm::rotate(Point - glm::vec3(0, 10, 0), Phase * glm::half_pi<std::float_t>(), glm::vec3(1.0f)),
-				glm::vec3(8, 8, 8)
+			glm::rotate(Point - glm::vec3(0, 10, 0), Phase * glm::half_pi<std::float_t>(), glm::vec3(1.0f)),
+			glm::vec3(8, 8, 8)
 		)),
 		Query
 	);
 
 	Query = Operations::Subtract(
 		glm::vec4(0, 0, 1, Shapes::Sphere(
-				Point - glm::vec3(0.0f, 10.0f, 0.0f),
-				10.0f
+			Point - glm::vec3(0.0f, 10.0f, 0.0f),
+			10.0f
 		)),
 		Query
 	);
 
 	Query = Operations::Union(
 		glm::vec4(1, 1, 0, Shapes::Torus(
-				glm::rotate(Point - glm::vec3(0, 5, 0), Phase * glm::pi<std::float_t>() / 2.0f, glm::vec3(1.0f)),
-				1.5f,
-				15.0f
+			glm::rotate(Point - glm::vec3(0, 5, 0), Phase * glm::pi<std::float_t>() / 2.0f, glm::vec3(1.0f)),
+			1.5f,
+			15.0f
 		)),
 		Query
 	);
@@ -71,8 +71,8 @@ glm::vec4 Scene(const glm::vec3& Point)
 		const std::float_t Completion = i / static_cast<std::float_t>(Count);
 		Query = Operations::Union(
 			glm::vec4(0, 1, 0, Shapes::Box(
-					Point - glm::vec3(Radius * glm::cos(2.0f * glm::pi<std::float_t>() * Completion), 5.0f, Radius * glm::sin(2.0f * glm::pi<std::float_t>() * Completion)),
-					glm::vec3(3, 10, 3)
+				Point - glm::vec3(Radius * glm::cos(2.0f * glm::pi<std::float_t>() * Completion), 5.0f, Radius * glm::sin(2.0f * glm::pi<std::float_t>() * Completion)),
+				glm::vec3(3, 10, 3)
 			)),
 			Query
 		);
@@ -80,25 +80,25 @@ glm::vec4 Scene(const glm::vec3& Point)
 
 	Query = Operations::Subtract(
 		glm::vec4(1, 0, 1, Shapes::Torus(
-				Point - glm::vec3(0, 8, 0),
-				3.0f,
-				25.0f
+			Point - glm::vec3(0, 8, 0),
+			3.0f,
+			25.0f
 		)),
 		Query
 	);
 
 	Query = Operations::Subtract(
 		glm::vec4(1, 0, 1, Shapes::Sphere(
-				Point - glm::vec3(glm::cos(glm::half_pi<std::float_t>() * Phase) * 25.0f, 2.0f, glm::sin(glm::half_pi<std::float_t>() * Phase) * 25.0f),
-				9.0f
+			Point - glm::vec3(glm::cos(glm::half_pi<std::float_t>() * Phase) * 25.0f, 2.0f, glm::sin(glm::half_pi<std::float_t>() * Phase) * 25.0f),
+			9.0f
 		)),
 		Query
 	);
 
 	Query = Operations::Union(
 		glm::vec4(1, 1, 1, Shapes::Sphere(
-				Point - glm::vec3(glm::cos(glm::half_pi<std::float_t>() * Phase) * 25.0f, 2.0f, glm::sin(glm::half_pi<std::float_t>() * Phase) * 25.0f),
-				3.0f
+			Point - glm::vec3(glm::cos(glm::half_pi<std::float_t>() * Phase) * 25.0f, 2.0f, glm::sin(glm::half_pi<std::float_t>() * Phase) * 25.0f),
+			3.0f
 		)),
 		Query
 	);
@@ -110,10 +110,10 @@ glm::vec3 CalcNormal(const glm::vec3& Point)
 {
 	const glm::vec2 Epsilon = glm::vec2(1.0, -1.0) * 0.5773f * 0.0005f;
 	return glm::normalize(glm::vec3(
-			Epsilon.xyy * Scene(Point + Epsilon.xyy).w +
-			Epsilon.yyx * Scene(Point + Epsilon.yyx).w +
-			Epsilon.yxy * Scene(Point + Epsilon.yxy).w +
-			Epsilon.xxx * Scene(Point + Epsilon.xxx).w
+		Epsilon.xyy * Scene(Point + Epsilon.xyy).w +
+		Epsilon.yyx * Scene(Point + Epsilon.yyx).w +
+		Epsilon.yxy * Scene(Point + Epsilon.yxy).w +
+		Epsilon.xxx * Scene(Point + Epsilon.xxx).w
 	));
 }
 
@@ -247,15 +247,15 @@ int main()
 	State.FOV = glm::tan(glm::radians(60.0f) / 2.0f);
 	const static std::size_t Cores = std::thread::hardware_concurrency();
 	std::thread Threads[Cores];
-	char Screen[ (WIDTH * 12 + 1) * HEIGHT + 1 ] = {};
+	char Screen[(WIDTH * 12 + 1) * HEIGHT + 1] = {};
 	do
 	{
 		const std::float_t Phase = Tick / static_cast<std::float_t>(DELAY);
 		State.EyePos = glm::vec3(35 * glm::sin(Phase * glm::pi<std::float_t>() / 10.0f), 10, 35 * glm::cos(Phase * glm::pi<std::float_t>() / 10.0f));
 		State.ViewMatrix = glm::inverse(glm::lookAt(
-				State.EyePos,
-				State.EyeAt,
-				glm::vec3(0.0f, 1.0f, 0.0f)
+			State.EyePos,
+			State.EyeAt,
+			glm::vec3(0.0f, 1.0f, 0.0f)
 		));
 		for( std::size_t i = 0; i < Cores; i++ )
 		{
@@ -276,7 +276,7 @@ int main()
 					Row,
 					"\e[1;3%u;%03um%c",
 					State.ScreenAttr[x + y * WIDTH] % 8,
-					State.ScreenAttr[x + y * WIDTH] & 0x80 ? (100 + State.ScreenAttr[x + y * WIDTH]%8):49,
+					State.ScreenAttr[x + y * WIDTH] & 0x80 ? (100 + State.ScreenAttr[x + y * WIDTH] % 8) : 49,
 					State.Screen[x + y * WIDTH]
 				);
 			}
